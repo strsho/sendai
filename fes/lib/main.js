b016ff00e8685526d191c4a89289f5a1373dae91
s.t.r. sho <shotaka@mbe.nifty.com>
2024-02-02 16:48:47 +0900

    Add files via upload

--- fes/lib/main.js
+++ fes/lib/main.js
@@ -1,4 +1,9 @@
 function make_fes(){
-  alert('make fes!');
-
+  oevent_name = document.getElementById('event_name');
+  oout = document.getElementById('output');
+  sret = '';
+  sret += oevent_name.value;
+  sret += 'が始まるよ'; 
+  oout.innerHTML = sret;
 }
+
