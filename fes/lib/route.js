daec95dac4faa9247ff1c96d8c68add7e388b57c
s.t.r. sho <shotaka@mbe.nifty.com>
2024-02-26 16:54:44 +0900

    Add files via upload

--- fes/lib/route.js
+++ fes/lib/route.js
@@ -1,9 +1,39 @@
 
+const style = {
+  'Point': new Style({
+    image: new CircleStyle({
+      fill: new Fill({
+        color: 'rgba(255,255,0,0.4)',
+      }),
+      radius: 5,
+      stroke: new Stroke({
+        color: '#ff0',
+        width: 1,
+      }),
+    }),
+  }),
+  'LineString': new Style({
+    stroke: new Stroke({
+      color: '#f00',
+      width: 3,
+    }),
+  }),
+  'MultiLineString': new Style({
+    stroke: new Stroke({
+      color: '#0f0',
+      width: 3,
+    }),
+  }),
+};
+
 const vector = new ol.layer.Vector({
   source: new ol.source.Vector({
     url: 'data/rk1.gpx',
     format: new ol.format.GPX(),
   }),
+  style: function (feature) {
+    return style[feature.getGeometry().getType()];
+  },
 });
 
 
